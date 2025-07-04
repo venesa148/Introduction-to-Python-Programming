b2666da8a0c98e2bda2657cd9eb3b8357bbee24b
venesa148 <venesahutajulu@gmail.com>
2025-07-04 19:01:41 +0700

    first commit
    first commit

---
+++ belajarpython.com/syntax_dasar.py
@@ -0,0 +1,42 @@
+print("Hellow world");
+# python itu case sennsitiv artinya huruf besar atau kecil memiliki perbedaan yang sangat berdampak
+# print tidak sama dengan PRINT tidak sama dengan Print
+
+# Mencetak nama
+print("Venesa")
+
+# Mencetak angka
+print(123)
+
+# tipe data boolean -> enggak perlu pakai kutip
+print(True)
+print(False)
+
+# tipe data string
+print("Hari ini aku sedang belajar pyhton sangat mudah")
+
+# tipe data integer
+print(21)
+
+# tipe data float pakai titik
+print(20.5)
+
+# tipe data Komplex
+print(9j)
+
+# Tipe data list
+print([1,2,3,4,5])
+print("satu","venesa","bisa")
+
+# Tipe data Tuple
+print((1,2,4))
+print(("satu","dua","tiga"))
+
+# Tipe data dictionary
+print({"nama" : "Venesa", "umur": 21})
+
+# Tipe data dictionary dimasukkan ke dalam variabel biodata
+biodata ={"nama":"Venesa"} 
+# proses cetak variabel sesuai dengan tipe data dictionary
+print(type(biodata))
+
---
+++ belajarpython.com/variabel.py
@@ -0,0 +1,18 @@
+nama = "Venesa"
+print(nama)
+
+umur = 21
+print(umur)
+# untuk mengetahui type dari variabel
+type(umur)
+
+# ubah nilai
+umur="tiga puluh tahun"
+print (umur)
+
+# contoh lain
+namaDepan = "Venesa"
+namaBelakang ="Herawaty"
+nama = namaDepan + "" + namaBelakang
+print(nama ,"\n" ,umur)
+# \n untuk new line
